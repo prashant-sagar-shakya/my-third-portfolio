3de25093cfc29c1560fc6e85a3ad4ad76e832c80
prashant-sagar-shakya <prashantshakya43602003@gmail.com>
2024-07-14 16:19:35 +0530

    Third Portfolio from JSM completed

---
+++ data/index.ts
@@ -0,0 +1,156 @@
+export const navItems = [
+  { name: "Home", link: "#" },
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "Skilled in Designing Engaging User Interfaces",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently Building an E-commerce Project Using JAM Stack",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Netflix",
+    des: "Netflix UI clone front page made by using HTML & Tailwind CSS",
+    img: "/p1.png",
+    iconLists: ["/html.webp", "tail.svg"],
+    link: "",
+  },
+  {
+    id: 2,
+    title: "Code-Alpha",
+    des: "An Online Code Editor Which Supports Coding Environments of Python, C, Cpp, JAVA.",
+    img: "/p2.png",
+    iconLists: ["/html.webp", "/css.png", "/js.svg", "/node.svg"],
+    link: "",
+  },
+  {
+    id: 3,
+    title: "BrainWave",
+    des: "A sleek, customizable and easy-to-use AI Chatbot Interface made by using JS, Tailwind & React.",
+    img: "/p3.png",
+    iconLists: ["/js.svg", "/tail.svg", "re.svg"],
+    link: "https://brainwave-ai-chat.netlify.app/",
+  },
+  {
+    id: 4,
+    title: "EcomEdge",
+    des: "An online e-commerce platform for buying products & goods made by using MERN stack.",
+    img: "/p4.png",
+    iconLists: ["tail.svg", "re.svg", "mongo.svg", "node.svg", "ex.png"],
+    link: "",
+  },
+  {
+    id: 5,
+    title: "Portfolio",
+    des: "A sleek, customizable, dynamic and attractive portfolio with a delight flavour of Framer-Motion.",
+    img: "/p5.png",
+    iconLists: ["tail.svg", "re.svg", "/next.svg", "/ts.svg"],
+    link: "https://my-first-portfolio-mu.vercel.app/",
+  },
+  {
+    id: 6,
+    title: "Imaginify",
+    des: "A SAAS based web application for image processing and manipulation.",
+    img: "/p6.png",
+    iconLists: ["tail.svg", "re.svg", "/next.svg", "/ts.svg", "mongo.svg"],
+    link: "https://imaginify-chi-two.vercel.app/",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "I.K Gujral Punjab Technical University (B.Tech-CSE)",
+    desc: "Currently pursuing Bachelor's Degree (4th Year Student) in Computer Science and Engineering",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Web & Python Full Stack Developer",
+    desc: "Completed 7+ projects in Web-Dev using React and Next Frameworks, Also developed gestures based applications using Python & OpenCV",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://www.github.com/prashant-sagar-shakya",
+  },
+  {
+    id: 2,
+    img: "/x.svg",
+    link: "https://x.com/Prashant_162003",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/prashant-shakya03/",
+  },
+];
